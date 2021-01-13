f334f99f635bf889c518d1136656e9cbec4cd1ba
moe <moe.faridd@gmail.com>
2021-01-13 01:05:44 -0500

    commit

--- app.js
+++ app.js
@@ -5,5 +5,36 @@ const app = new Vue ({
         JWT: "",
         createUN: "", 
         createPW: "",
+        devURL: "http://localhost:3000",
+        prodURL: null,
+        user: null, 
+        token: null, 
+    },
+    methods: {
+        handleLogin: function() {
+            const URL = this.prodURL ? this.prodURL : this.devURL;
+            const user = { username: this.createUN, password: this.createPW };
+            console.log("hello");
+            fetch(`${URL}/login`, {
+                method: "POST", 
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            })
+            .then((response) => response.json())
+            .then((data) => {  
+            this.user = data.user;    
+            this.token = data.token;     
+            this.loggedin = true;   
+            this.createPW=""
+            this.createUN=""
+        });
+        },
+        handleLogout: function() {
+            this.loggedin= false
+            this.user= null;
+            this.token = null;
+        }
     }
 })
